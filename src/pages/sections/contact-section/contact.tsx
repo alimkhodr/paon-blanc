f7feff025db0f351cdc0c2be858179977f2ecc46
alimkhodr <ali.m.khodr@gmail.com>
2025-01-05 22:08:43 -0300

    feat: add border in maps

--- src/pages/sections/contact-section/contact.tsx
+++ src/pages/sections/contact-section/contact.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Container, IconButton, styled, Typography } from '@mui/mat
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
+import theme from '../../../theme';
 
 const Contact = () => {
     const StyledContact = styled('div')(({ theme }) => ({
@@ -17,7 +18,7 @@ const Contact = () => {
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d229.17333992172425!2d-45.908833018473416!3d-23.21492590240116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cc35ab7fcf9253%3A0x5b698e4f060e2154!2sPromovere%20Vita%20-%20Coworking!5e0!3m2!1spt-BR!2sbr!4v1736117182280!5m2!1spt-BR!2sbr"
                         width="100%"
                         height="300"
-                        style={{ border: 0 }}
+                        style={{ border: `solid ${theme.palette.background.default} 3px`, borderRadius: 5 }}
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
                     />
