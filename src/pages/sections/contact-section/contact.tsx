8690835a48c31a8872109b7889b73c4b927c6d28
alimkhodr <ali.m.khodr@gmail.com>
2025-01-21 21:21:44 -0300

    style: add backgroud in icons

--- src/pages/sections/contact-section/contact.tsx
+++ src/pages/sections/contact-section/contact.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Container, styled, Typography } from '@mui/material';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import theme from '../../../theme';
 
 const Contact = () => {
     const StyledContact = styled('div')(({ theme }) => ({
@@ -26,7 +27,7 @@ const Contact = () => {
                             <Typography variant="h5" fontWeight="bold">Contato</Typography>
                             <Button
                                 size="large"
-                                startIcon={<WhatsAppIcon sx={{ color: "#2ba048" }} />}
+                                startIcon={<WhatsAppIcon sx={{ color: theme.palette.background.paper, backgroundColor: theme.palette.primary.main, padding: "3px", borderRadius:1}} />}
                                 sx={{ padding: '0 5px' }}
                                 onClick={() => window.open('https://wa.me/5512996119002?text=Olá!', '_blank')}
                             >
@@ -34,7 +35,7 @@ const Contact = () => {
                             </Button>
                             <Button
                                 size="large"
-                                startIcon={<MailOutlineIcon sx={{ color: "#2ba048" }} />}
+                                startIcon={<MailOutlineIcon sx={{ color: theme.palette.background.paper, backgroundColor: theme.palette.primary.main, padding: "3px", borderRadius:1}} />}
                                 sx={{ padding: '0 5px', textTransform: 'none' }}
                                 onClick={() => window.open('mailto:paonblancestetica@gmail.com', '_blank')}
                             >
@@ -57,7 +58,7 @@ const Contact = () => {
                             <Typography variant="h5" fontWeight="bold">Redes Sociais</Typography>
                             <Button
                                 size="large"
-                                startIcon={<InstagramIcon sx={{ color: "#2ba048" }} />}
+                                startIcon={<InstagramIcon sx={{ color: theme.palette.background.paper, backgroundColor: theme.palette.primary.main, padding: "3px", borderRadius:1}} />}
                                 sx={{ padding: '0 5px', textTransform: 'none' }}
                                 onClick={() => window.open('https://www.instagram.com/paonblancestetica', '_blank')}
                             >
